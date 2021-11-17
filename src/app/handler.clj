c41fd0e8b6a50af23837547581dcfc69f0074103
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2021-11-17 17:21:41 -0300

    implement and test webservice for the scrambled words detection

--- src/app/handler.clj
+++ src/app/handler.clj
@@ -1,11 +1,21 @@
 (ns app.handler
   (:require
+    [app.core :as s]
+    [clojure.data.json :as json]
     [compojure.core :as cpj]
     [compojure.route :as route]
-    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+    [ring.util.response :as r]))
+
+(defn scramble? [scrambled-word word]
+  (r/response
+    (json/write-str {:scrambled
+                       (s/scramble? scrambled-word word)})))
 
 (cpj/defroutes app-routes
-  (cpj/GET "/" [] "Hello World")
+  (cpj/GET "/scramble/:scrambled-word/:word"
+           [scrambled-word word]
+           (scramble? scrambled-word word))
   (route/not-found "Not Found"))
 
 (def app
