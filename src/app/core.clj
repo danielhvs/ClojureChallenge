c8717f0811e531be6501c673f5d4bcb4065b4a98
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2021-11-16 23:09:46 -0300

    using threading macro and renaming predicate again

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,14 +1,16 @@
 (ns app.core)
 
-(def less-than-or-equal-zero? (complement pos?))
+(def zero-or-less? (complement pos?))
 
 (defn scramble?
   "returns true if a portion of scrambled-word characters can be rearranged to match word, otherwise returns false"
   [scrambled-word word]
   (let [scrambled-freqs (frequencies scrambled-word)
-        word-freqs (frequencies word)]
+        word-freqs (frequencies word)
+        word-chars (keys word-freqs)]
     (every?
-      less-than-or-equal-zero?
-      (vals
-        (merge-with - word-freqs
-                    (select-keys scrambled-freqs (keys word-freqs)))))))
+      zero-or-less?
+      (->> word-chars
+           (select-keys scrambled-freqs)
+           (merge-with - word-freqs)
+           vals))))
