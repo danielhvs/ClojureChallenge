e7d99b858e85e971a7df76665a67c4c5aceb70aa
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2021-11-16 23:26:49 -0300

    preconditions approach can be considered as a boundary limitation

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,10 +1,14 @@
 (ns app.core)
 
 (def zero-or-less? (complement pos?))
+(def only-lower-case-letters? (partial re-matches #"[a-z]+"))
 
 (defn scramble?
-  "returns true if a portion of scrambled-word characters can be rearranged to match word, otherwise returns false"
+  "Returns true if a portion of scrambled-word characters can be rearranged to match word, otherwise returns false.
+   Only lower case letters will be used (a-z). No punctuation or digits will be included."
   [scrambled-word word]
+  {:pre [(and (only-lower-case-letters? word)
+              (only-lower-case-letters? scrambled-word))]}
   (let [scrambled-freqs (frequencies scrambled-word)
         word-freqs (frequencies word)
         word-chars (keys word-freqs)]
--- test/app/core_test.clj
+++ test/app/core_test.clj
@@ -4,6 +4,13 @@
     [clojure.test :refer [deftest testing is]]))
 
 (deftest scrambled
+  (testing "Invalid input"
+    (is (thrown? java.lang.AssertionError
+                 (app/scramble? "" "")))
+    (is (thrown? java.lang.AssertionError
+                 (app/scramble? "123" "abc")))
+    (is (thrown? java.lang.AssertionError
+                 (app/scramble? "a.c" "abc"))))
   (testing "Scrambled words"
     (is (true? (app/scramble? "rekqodlw" "world")))
     (is (true? (app/scramble? "cedewaraaossoqqyt" "codewars"))))
