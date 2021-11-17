1c6256a476e1cd526c648ef6806f603dc1a596f6
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2021-11-16 23:47:00 -0300

    refactoring test code and adding more cases

--- test/app/core_test.clj
+++ test/app/core_test.clj
@@ -3,14 +3,20 @@
     [app.core :as app]
     [clojure.test :refer [deftest testing is]]))
 
+(def valid-input "abc")
+
+(defn test-invalid-input [scrambled-word word]
+  (is (thrown? java.lang.AssertionError
+               (app/scramble? scrambled-word word))))
+
 (deftest scrambled
   (testing "Invalid input"
-    (is (thrown? java.lang.AssertionError
-                 (app/scramble? "" "")))
-    (is (thrown? java.lang.AssertionError
-                 (app/scramble? "123" "abc")))
-    (is (thrown? java.lang.AssertionError
-                 (app/scramble? "a.c" "abc"))))
+    (test-invalid-input valid-input "")
+    (test-invalid-input valid-input "123")
+    (test-invalid-input valid-input "a.c")
+    (test-invalid-input "" valid-input)
+    (test-invalid-input "123" valid-input)
+    (test-invalid-input "a.c" valid-input))
   (testing "Scrambled words"
     (is (true? (app/scramble? "rekqodlw" "world")))
     (is (true? (app/scramble? "cedewaraaossoqqyt" "codewars"))))
