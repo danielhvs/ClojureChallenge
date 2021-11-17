740861050e7c25eb87d61ddaf51a000bda2ef46e
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2021-11-16 22:58:32 -0300

    refactor rename the function so it's more clear it is a predicate

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,7 +1,6 @@
 (ns app.core)
 
-(defn less-than-or-equal-zero []
-  (complement pos?))
+(def less-than-or-equal-zero? (complement pos?))
 
 (defn scramble?
   "returns true if a portion of scrambled-word characters can be rearranged to match word, otherwise returns false"
@@ -9,7 +8,7 @@
   (let [scrambled-freqs (frequencies scrambled-word)
         word-freqs (frequencies word)]
     (every?
-      (less-than-or-equal-zero)
+      less-than-or-equal-zero?
       (vals
         (merge-with - word-freqs
                     (select-keys scrambled-freqs (keys word-freqs)))))))
