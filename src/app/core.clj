9c318d532abf29b0dc48da7d1246a7775c4f0653
Daniel Habib Vieira da Silva <danielhvs70@gmail.com>
2021-11-16 22:33:32 -0300

    implementation using frequencies

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,6 +1,16 @@
 (ns app.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn scramble?
+  "returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false"
+  [str1 str2]
+  (let [mapa1 (frequencies str1)
+        mapa2 (frequencies str2)]
+    (every?
+      (complement pos?)
+      (vals
+        (merge-with - mapa2
+                    (select-keys mapa1 (keys mapa2)))))))
+
+(scramble? "rekqodlw" "world") ; ==> true
+(scramble? "cedewaraaossoqqyt" "codewars") ;  ==> true
+(scramble? "katas" "steak") ; ==> false
